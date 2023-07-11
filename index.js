81fd0e7bf9358950a42b2710b77989520cad274b
joao vieira <john.collins.vieira@gmail.com>
2023-07-11 09:42:25 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// Write your code here
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id','victory');
+newHeader.textContent = "LeVi is the champion";
+document.body.append(h1);
+
+
+
+
+
+
